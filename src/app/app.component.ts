fa063c5aa588cf77c8c6176f596bdbfa9a6e9015
waikit2365 <waikit2365@gmail.com>
2019-08-08 08:10:51 +0800

    ShoppingCart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ShoppingCart';
+  products = [
+    {
+      id: "samsung",
+      price: 1000,
+      description: "Samsung Phone", 
+      image: "./product_samsung.jpg"
+    },
+    {
+      id: "xiaomi",
+      price: 2000,
+      description: "Xiaomi Phone", 
+      image: "./product_xiaomi.jpg"
+    },
+    {
+      id: "apple",
+      price: 3000,
+      description: "Apple Phone", 
+      image: "./product_apple.jpg"
+    },
+  ]
+
+  showProduct (){
+    for(let i:number = 0; i < this.products.length; i++){
+      document.getElementById("image").innerHTML = this.products[i].image;
+      document.getElementById("id").innerHTML = "ID: " + this.products[i].id;
+      document.getElementById("price").innerHTML = "Price: " + this.products[i].price;
+      document.getElementById("description").innerHTML = "Description: " + this.products[i].description;
+    }
+  }
 }
